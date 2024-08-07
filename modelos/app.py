907122d7604bc535dd11800ed1f00612e5e67485
Akageluv <akageluv@gmail.com>
2024-08-07 15:49:41 -0300

    Commit

---
+++ modelos/app.py
@@ -0,0 +1,12 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Gui', 5)
+restaurante_praca.receber_avaliacao('Lais', 3)
+restaurante_praca.receber_avaliacao('Emy', 2)
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
